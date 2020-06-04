c3a59eea3316b4f863d91deb8853791bcfbe040e
pcchiu96 <pcchiu96@gmail.com>
2020-06-03 18:33:46 -0700

    Added my projects section using grid and similar style from before. Added linkdin and github icon next to title name along with their links. Added animations to all buttons.

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,15 @@ export default function Nav() {
     return (
         <nav id='nav'>
             <ul>
-                <li className='nav-title'>Paul Chiu</li>
+                <li className='nav-title'>
+                    <a href='https://pcchiu96.github.io/portfolio'>Paul Chiu</a>
+                    <a href='https://www.linkedin.com/in/paul-chiu-373616178'>
+                        <i className='fa fa-linkedin-square' aria-hidden='true'></i>
+                    </a>
+                    <a href='https://github.com/pcchiu96'>
+                        <i className='fa fa-github' aria-hidden='true'></i>
+                    </a>
+                </li>
                 <li className='nav-hamburger'>
                     <i className='fa fa-bars'></i>
                 </li>
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,35 +3,45 @@ import "./projects.css";
 
 export default function Projects() {
     return (
-        <div className='projects-wrapper'>
-            <section className='projects'>
-                <section>
-                    <i className='fa fa-angle-down'></i>
-                    <h3>
-                        <a href='./unlock-code/src/index.js'>Unlock Code</a>
-                    </h3>
-                    <p>This is just a fun little project I made to see if I can replicate what Snowden said about passwords under 8 characters can be solved under a second.</p>
-                </section>
-                <section>
-                    <i className='fa fa-angle-down'></i>
-                    <h3>
-                        <a href='./unlock-code/src/index.js'>Connect 4</a>
-                    </h3>
-                    <p>A 2 players board game that I built to practice coding in ReactJS.</p>
-                </section>
-                <section>
-                    <i className='fa fa-angle-down'></i>
-                    <h3>
-                        <a href='https://www.tigerapplicationsllc.com/index.php?page=index'>Tiger Applications LLC</a>
-                    </h3>
-                    <p>A work in progress website I designed and built for Tiger Applications LLC.</p>
-                </section>
-                <section>
-                    <i className='fa fa-angle-down'></i>
-                    <h3>StrataPLBm</h3>
-                    <p>A client-server application that provide simple management of condo/apartment requests from residents.</p>
-                </section>
-            </section>
+        <div className='projects-background'>
+            <div className='projects-wrapper'>
+                <div className='projects'>
+                    <section>
+                        <i className='fa fa-gamepad' aria-hidden='true'></i>
+                        <h3>
+                            <a href='https://github.com/pcchiu96/Werewolf-Bot'>Discord Werewolf Bot</a>
+                        </h3>
+                        <p>A discord werewolf game that's similar to mafia but with a fun twist.</p>
+                    </section>
+                    <section>
+                        <i className='fa fa-square-o' aria-hidden='true'></i>
+                        <h3>
+                            <a href='https://github.com/pcchiu96/Connect_4'>Connect 4</a>
+                        </h3>
+                        <p>A 2-players board game that I built to practice coding in ReactJS.</p>
+                    </section>
+                    <section>
+                        <i className='fa fa-unlock-alt' aria-hidden='true'></i>
+                        <h3>
+                            <a href='https://github.com/pcchiu96/Unlock_Code'>Unlock Code</a>
+                        </h3>
+                        <p>This is just a fun little project I made to see if I can replicate what Snowden said about passwords under 8 characters can be solved under a second.</p>
+                    </section>
+
+                    <section>
+                        <i className='fa fa-wrench' aria-hidden='true'></i>
+                        <h3>
+                            <a href='https://www.tigerapplicationsllc.com/index.php?page=index'>Tiger Applications LLC</a>
+                        </h3>
+                        <p>A work in progress website I designed and built for Tiger Applications LLC.</p>
+                    </section>
+                    <section>
+                        <i className='fa fa-wrench' aria-hidden='true'></i>
+                        <h3>StrataPLBm</h3>
+                        <p>A client-server application that provide simple management of condo/apartment requests from residents.</p>
+                    </section>
+                </div>
+            </div>
         </div>
     );
 }
