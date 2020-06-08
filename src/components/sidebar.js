0d5c3a4fa0d8fa3b8004456d48b5bbdd0bb70d58
pcchiu96 <pcchiu96@gmail.com>
2020-06-07 21:16:22 -0700

    Added hamburger slide in animation but still need more work.

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,32 +1,27 @@
-import React, { useState } from "react";
-import Sidebar from "./sidebar";
-import "./nav.css";
+import React from "react";
+import "./sidebar.css";
 
-export default function Nav() {
-    const [sidebar, setSideBar] = useState(false);
+export default function Sidebar({ show }) {
+    let sidebarClass = "sidebar";
+    if (show) sidebarClass += " show-sidebar";
 
     return (
-        <nav id='nav'>
+        <nav className={sidebarClass}>
             <ul>
-                <li className='nav-title'>
+                <li>
                     <a href='https://pcchiu96.github.io/portfolio'>Paul Chiu</a>
+                </li>
+                <li>
                     <a href='https://www.linkedin.com/in/paul-chiu-373616178'>
                         <i className='fa fa-linkedin-square' aria-hidden='true'></i>
                     </a>
+                </li>
+                <li>
                     <a href='https://github.com/pcchiu96'>
                         <i className='fa fa-github' aria-hidden='true'></i>
                     </a>
                 </li>
-                <li className='nav-hamburger'>
-                    <i className='fa fa-bars' onClick={showSidebar}></i>
-                </li>
             </ul>
-
-            <Sidebar show={sidebar} />
         </nav>
     );
-
-    function showSidebar() {
-        setSideBar(!sidebar);
-    }
 }
