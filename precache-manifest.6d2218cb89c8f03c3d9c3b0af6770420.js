b5182fe5fcede67864b8f0e631d7241d26404db9
pcchiu96 <pcchiu96@gmail.com>
2020-06-17 00:07:04 -0700

    Updates

---
+++ precache-manifest.6d2218cb89c8f03c3d9c3b0af6770420.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91878f1fe2832a75b447318457847237",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "310f14fab029ba8b4ee7",
+    "url": "/portfolio/static/css/main.9b53c6bd.chunk.css"
+  },
+  {
+    "revision": "cae730c948e250dff93e",
+    "url": "/portfolio/static/js/2.a61c362d.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.a61c362d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310f14fab029ba8b4ee7",
+    "url": "/portfolio/static/js/main.3f15ce68.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "78a99822ddfca9c82d45721870283b22",
+    "url": "/portfolio/static/media/background.78a99822.jpg"
+  },
+  {
+    "revision": "dfd2b2bdc0a22b0d2a4ced229e517c66",
+    "url": "/portfolio/static/media/resume.dfd2b2bd.pdf"
+  }
+]);
