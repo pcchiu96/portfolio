c729f4af77ed144d5c3da1f77787f9561b241750
pcchiu96 <pcchiu96@gmail.com>
2020-09-14 12:13:21 -0700

    Updates

---
+++ precache-manifest.78cacd35221a36ef9b318217b75c2375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79914c28ffc3a9476e31300c6eec9c3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "20b3263231f48c9dfb44",
+    "url": "/portfolio/static/css/main.f8617b2a.chunk.css"
+  },
+  {
+    "revision": "c18bf85afddd98980166",
+    "url": "/portfolio/static/js/2.db189d7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.db189d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b3263231f48c9dfb44",
+    "url": "/portfolio/static/js/main.4166328c.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "78a99822ddfca9c82d45721870283b22",
+    "url": "/portfolio/static/media/background.78a99822.jpg"
+  },
+  {
+    "revision": "ae879e8c62ec9251e433ba7f4d266161",
+    "url": "/portfolio/static/media/fm_favicon.ae879e8c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.367dfdf8c935e66a26ba039cb04c9eef.js"
+  "/portfolio/precache-manifest.78cacd35221a36ef9b318217b75c2375.js"
 );
 
 self.addEventListener('message', (event) => {
